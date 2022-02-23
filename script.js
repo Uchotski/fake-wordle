70e9e4ccbfd7b9613920e3988a30a6bca7b6093d
Uchotski <smith.uchotski@gmail.com>
2022-02-23 21:43:08 +0000

    Make the game functional.

--- script.js
+++ script.js
@@ -1,91 +1,83 @@
 //PAGE COMPONENTS:
 //Toggle Overlay...
-document.getElementsByClassName('close-button')[0].onclick = function() {
+document.getElementsByClassName('close-button')[0].onclick = function () {
     document.getElementById('overlay').style.display = 'none';
 }
 
-document.getElementById('menu').onclick = function() {
+document.getElementById('menu').onclick = function () {
     document.getElementById('overlay').style.display = 'flex';
 }
 
-//Functional Keyboard:
-//Making the Keyboard Functional
-const keyboardPress = () => {
-    let content = event.target.innerHTML;
-    if (content.length == 1) {
-        console.log(content);
-        console.log("A single character was pressed!");
-    } else if (content == 'delete') {
-        console.log("The delete button was pressed!");
-    } else if (content == 'enter') {
-        console.log("The enter button was pressed!");
-    } else {
-        return console.log("Something else got pushed!");
-    }
-}
-
-document.getElementById('keyboard').addEventListener("click", keyboardPress);
-
 //GAME ELEMENTS:
-//Random Word Generator:
-const makeWord = () => {
-    const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    const newWord = [];
-    let i = 0;
-    while (i < 5) {
-        newWord.push(alphabet[Math.floor(Math.random() * 26)]);
-        i++;
+//Starts the game...
+const startGame = () => {
+    //Function to generate new word...
+    const makeWord = (length) => {
+        const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+        const newWord = [];
+        let i = 0;
+        while (i < length) {
+            newWord.push(alphabet[Math.floor(Math.random() * 26)]);
+            i++;
+        }
+        return newWord;
     }
 
-    console.log(newWord.join(""));
-    return newWord.join("");
-}
+    //Function to make keyboard functional...
+    const typeArea = () => {
+        const gameArea = document.getElementById('game-grid');
+        let content = event.target.innerHTML;
 
-let col = 0;
+        if (content.length == 1) {
+            //If one letter is selected...
+            if (col >= maxCol || row >= maxRow) { return; }
+            gameArea.children[row].children[col].innerHTML = content;
+            return col++;
+        } else if (content == 'delete') {
+            //If 'delete' is clicked...
+            if (col <= 0) { return; }
+            col--;
+            return gameArea.children[row].children[col].innerHTML = "";
+        } else if (content == 'enter') {
+            //If 'enter' is clicked...
+            if (col != maxCol) { return; }
+            return submitGuess(collectGuess());
+        } else {
+            return;
+        }
+    }
+
+    //Function to collect guesses...
+    const collectGuess = () => {
+        const currentRow = Array.from(document.getElementById('game-grid').children[row].children);
+        const guess = [];
+        currentRow.forEach(item => guess.push(item.innerHTML));
+        return guess;
+    }
+
+    //Function to handle what happens when a user submits a guess...
+    const submitGuess = (guess) => {
+        console.log(`Hello! The current guess is ${guess.join("")}!`);
+        console.log(`The current word to guess is ${word.join("")}!`);
+        if (guess.join("") == word.join("")) {
+            document.getElementById('keyboard').removeEventListener("click", typeArea);
+            return console.log("You won!");
+        } else {
+            row++;
+            col = 0;
+            return console.log("Try again!");
+        }
+    }
+
+    //Game Variables:
+    let col = 0;
     let row = 0;
     let maxCol = 5;
-    let maxRow = 5;
+    let maxRow = 6;
+    let word = makeWord(maxCol); //Generates and stores a random word.
 
-const editGameArea = () => {
-    const gameArea = document.getElementById('game-grid');
-
-    //Keyboard press...
-    let content = event.target.innerHTML;
-    let keypress;
-    if (content.length == 1) {
-        console.log(content);
-        console.log("A single character was pressed!");
-        keypress = content;
-    } else if (content == 'delete') {
-        console.log("The delete button was pressed!");
-        keypress = content;
-    } else if (content == 'enter') {
-        console.log("The enter button was pressed!");
-        keypress = content;
-    } else {
-        console.log("Something else got pushed!");
-    }
-
-    if (keypress.length == 1) {
-        if (col > maxCol || row > maxRow) { return; }
-        console.log(keypress);
-        gameArea.children[row].children[col].innerHTML = keypress;
-        col++;
-        return;
-    } else if (keypress == 'delete') {
-        if (col <= 0) { return; }
-        col--;
-        gameArea.children[row].children[col].innerHTML = "";
-        return;
-    } else if (keypress == 'enter') {
-        console.log(col);
-        if (col != maxCol) { return console.log("You hit enter too soon!"); }
-        console.log("The enter button was hit!");
-        row++;
-        col = 0;
-        return;
-    }
+    //Enables the keyboard...
+    document.getElementById('keyboard').addEventListener("click", typeArea);
 }
 
-document.getElementById("keyboard").addEventListener("click", editGameArea);
-document.getElementById("start").addEventListener("click", makeWord);
+document.getElementById('start').addEventListener("click", startGame);
